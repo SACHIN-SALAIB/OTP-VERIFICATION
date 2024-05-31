13b213b71d3e834d2a804589f7dde0e96954db5c
Sachin salaib <sachinsalaib640@gmail.com>
2024-05-31 17:47:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Otp from './Components/Otp';
+import Otppage from './Components/Otppage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App ">
+     <Otppage/>
+    </div>
+  );
+}
+
+export default App;
